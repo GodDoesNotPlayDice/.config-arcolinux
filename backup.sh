a30bd7df803bfb69d29b46b644e51d890f639698
GodDoesNotPlayDice <104604407+GodDoesNotPlayDice@users.noreply.github.com>
2024-03-04 15:03:57 -0300

    add(backup.sh): init

---
+++ backup.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Function to check the result of each command
+check_result() {
+    if [ $? -ne 0 ]; then
+        echo "Error occurred: $1"
+        exit 1
+    fi
+}
+
+check_directory() {
+    if [ -d "$1" ]; then
+        echo "Directory '$1' already exists."
+    else
+        echo "Directory '$1' created successfully."
+    fi
+}
+
+# Attempt to create directories
+
+# mkdir -p ~/Documents/GitHub/.config-arcolinux/hypr/
+# check_directory ~/Documents/GitHub/.config-arcolinux/hypr/
+
+# Backup configurations
+
+
+cp -r ~/.config/hypr/ ~/Documents/GitHub/.config-arcolinux/
+check_result "Failed to backup hypr/"
+
+cp -r ~/.config/alacritty/ ~/Documents/GitHub/.config-arcolinux
+check_result "Failed to backup alacritty/"
+
+cp -r ~/.config/waybar/ ~/Documents/GitHub/.config-arcolinux
+check_result "Failed to backup waybar"
+
+cp -r ~/.config/neofetch/ ~/Documents/GitHub/.config-arcolinux
+check_result "Failed to backup neofetch/"
+
+cp -r ~/.config/ulauncher/ ~/Documents/GitHub/.config-arcolinux
+check_result "Failed to backup ulauncher/"
+
+# MD
+cp -r ~/Documents/config.md/README.md ~/Documents/GitHub/.config-arcolinux
+check_result "Failed to backup config.md/README.md"
+
+echo "All files backed up successfully."
